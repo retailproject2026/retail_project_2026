791504b0774cc5b163a72f56e3a27e946039d645
paventhan <paventhanbtech1990@gmail.com>
2026-09-21 22:14:47 +0530

    changes added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://retail-project-backend.onrender.com/api',
+  supabaseUrl: 'https://jmiqhsvhqgrxbybkdkst.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Sif-xcep73voehAYj4lLvQ_zVJhW0X-'
+};
