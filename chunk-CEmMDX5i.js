20a646980fe218e1d78c4c27a880aad4cde90d9e
paventhan <paventhanbtech1990@gmail.com>
2026-09-22 16:09:58 +0530

    Auto-generated commit

--- chunk-CEmMDX5i.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,apiUrl:`https://retail-project-backend.onrender.com/api`,supabaseUrl:`https://jmiqhsvhqgrxbybkdkst.supabase.co`,supabaseAnonKey:`sb_publishable_Sif-xcep73voehAYj4lLvQ_zVJhW0X-`};export{e as t};
